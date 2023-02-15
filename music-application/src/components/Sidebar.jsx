5a51c8df99117e5500962f573ed7b7c87832b77e
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-14 23:42:09 -0600

    Added Header, Footer, and Sidebar components to index.js and added component CSS

--- music-application/src/components/Sidebar.jsx
+++ music-application/src/components/Sidebar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from './Sidebar.module.css'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className={styles.sidebar}>Sidebar</div>
   )
 }
 
--- music-application/src/index.js
+++ music-application/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
+    <Sidebar />
     <App />
+    <Footer />
   </React.StrictMode>
 );
 
