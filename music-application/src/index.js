496c9574f03db2d21e8fd682f52cf2a6272df9fa
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-14 23:50:42 -0600

    Adder individual component folders for Header, Footer, and Sidebar and changed import links

--- music-application/src/index.js
+++ music-application/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import Footer from './components/Footer';
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
+import Footer from './components/Footer/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
